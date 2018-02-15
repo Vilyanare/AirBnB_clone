72faace77790de608990bc8fa5380e0be2e7322e
Thomas <vilyanare@Thomass-MacBook-Pro.local>
2018-02-14 17:25:40 -0800

    Added attribute checks for FIleStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -12,14 +12,15 @@ class TestFileStorage(unittest.TestCase):
     """Utilizes unittest to evaluate possible outcomes of
     creating instances of FileStorage class
     """
-    my_path = models.storage._FileStorage__file_path
 
     def setUp(self):
         """sets up testing environment so previous file storage
         is not affected"""
         file_path = models.storage._FileStorage__file_path
+        self.my_path = models.storage._FileStorage__file_path
         if os.path.exists(file_path):
-            os.rename(file_path, 'test_file_storage')
+            os.rename(file_path, 'test_storage')
+        self.testcase = FileStorage()
 
     def tearDown(self):
         """ Removes JSON file after test cases run """
@@ -28,21 +29,20 @@ class TestFileStorage(unittest.TestCase):
             os.remove(file_path)
         if os.path.exists('test_storage'):
             os.rename('test_storage', file_path)
+        self.testcase = None
 
     def test_save_1(self):
         """testing if save saves new instance properly
         """
-        my_path = models.storage._FileStorage__file_path
         my_model = BaseModel()
         my_storage = FileStorage()
         my_storage.new(my_model)
         my_storage.save()
-        file_existence = os.path.exists(my_path)
+        file_existence = os.path.exists(self.my_path)
         self.assertEqual(True, file_existence)
 
     def test_save_2(self):
         """tests if updating attributes saves properly"""
-        my_path = models.storage._FileStorage__file_path
         my_model = BaseModel()
         my_storage = FileStorage()
         my_model.my_number = 89
@@ -51,12 +51,11 @@ class TestFileStorage(unittest.TestCase):
 
     def test_save_3(self):
         """tests if file contains correct id after saving"""
-        my_path = models.storage._FileStorage__file_path
         my_model = BaseModel()
         my_storage = FileStorage()
         my_storage.save()
         desired_str = 'BaseModel.' + my_model.id
-        with open(my_path, "r", encoding="utf-8") as f:
+        with open(self.my_path, "r", encoding="utf-8") as f:
             if desired_str in f.read():
                 result = True
             else:
@@ -65,18 +64,24 @@ class TestFileStorage(unittest.TestCase):
 
     def test_file_path(self):
         """testing if file saves as correct file path name"""
-        my_path = models.storage._FileStorage__file_path
         my_storage = FileStorage()
         my_storage.save()
-        expected_name = my_path
+        expected_name = self.my_path
         self.assertEqual(expected_name, my_storage._FileStorage__file_path)
 
     def test_file_empty(self):
         """testing if file saves properly, and doesn't return an
         empty file"""
-        my_path = models.storage._FileStorage__file_path
         my_model = BaseModel()
         my_storage = FileStorage()
         my_storage.new(my_model)
         my_storage.save()
-        self.assertEqual(os.stat(my_path).st_size == 0, False)
+        self.assertEqual(os.stat(self.my_path).st_size == 0, False)
+
+    def test_file_storage_file_path_exists(self):
+        """Tests if __file_path is created correctly"""
+        self.assertEqual(type(self.testcase._FileStorage__file_path), str)
+
+    def test_file_storage_objects_exists(self):
+        """Test if __objects is created correctly"""
+        self.assertEqual(type(self.testcase.all()), dict)
